8e8aac76e356523c6284734de3d0221c1f7e7069
Danilo Gonçalves <gonca.danilo@gmail.com>
2022-02-10 11:22:48 -0300

    feat: use labels dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import './App.scss';
+import { useEffect, useState } from 'react';
+import { useIntl } from 'react-intl';
+import { getLanguage, setLanguage, Language } from 'src/helpers/useLanguage';
+import 'src/App.scss';
 
 export function App() {
+  const { formatMessage } = useIntl();
+  const language = getLanguage();
+
+  const [currentLanguage, setCurrentLanguage] = useState<Language>(language);
+
+  useEffect(() => {
+    setLanguage(currentLanguage);
+  }, [currentLanguage]);
+
+  function handleChangeLanguage(lang: string) {
+    setCurrentLanguage(lang as Language);
+    window.location.reload();
+  }
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <p>
-        This is a simple example of a React application with multi languages.
-      </p>
+      <h1>{formatMessage({ id: 'app.title' })}</h1>
+      <p>{formatMessage({ id: 'app.description' })}</p>
 
-      <select name="language" id="language">
-        <option value="en">EN</option>
+      <select
+        value={currentLanguage}
+        onChange={(event) => handleChangeLanguage(event.target.value)}
+      >
         <option value="pt-BR">PT-BR</option>
+        <option value="en">EN</option>
+        <option value="es">ES</option>
       </select>
     </div>
   );
