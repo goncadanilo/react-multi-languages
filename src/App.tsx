85d99fc3d48ff28d3f60dd080d651ab057dab332
Danilo Gonçalves <gonca.danilo@gmail.com>
2022-02-10 10:21:42 -0300

    feat: create the project base

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import './App.scss';
+
 export function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <p>
+        This is a simple example of a React application with multi languages.
+      </p>
+
+      <select name="language" id="language">
+        <option value="en">EN</option>
+        <option value="pt-BR">PT-BR</option>
+      </select>
     </div>
   );
 }
