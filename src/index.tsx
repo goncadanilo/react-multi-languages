defac1bf7f341faeda134445f97093021f59f78c
Danilo Gonçalves <gonca.danilo@gmail.com>
2022-02-10 11:21:49 -0300

    feat: set IntlProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import { IntlProvider } from 'react-intl';
+import { App } from 'src/App';
+import { getLanguage } from 'src/helpers/useLanguage';
+import ptBR from 'src/locales/pt-BR.json';
+import en from 'src/locales/en.json';
+import es from 'src/locales/es.json';
+
+const messages = { 'en': en, 'pt-BR': ptBR, 'es': es };
+const language  = getLanguage();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <IntlProvider locale={language} messages={messages[language]}>
     <App />
-  </React.StrictMode>,
+  </IntlProvider>,
   document.getElementById('root')
 );
